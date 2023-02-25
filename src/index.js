4508fd437c2a06ba57f301388d90344095c397ae
Yettytolu <yettyogunremi@gmail.com>
2023-02-24 22:00:43 -0500

    Added the forecast API to the weather app

--- src/index.js
+++ src/index.js
@@ -1,68 +1,76 @@
-let now = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-//let months = [
-//"January",
-//"February",
-//"March",
-//"April",
-//"May",
-//"June",
-//"July",
-//"August",
-//"September",
-//"October",
-//"November",
-//"December",
-//];
-//let month = months[now.getMonth()];
-//let year = [now.getFullYear()];
-//let date = [now.getDate()];
-let hours = [now.getHours()];
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let minutes = [now.getMinutes()];
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-let today = document.querySelector("#sun");
-today.innerHTML = `${day}, ${hours}:${minutes}`;
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = [date.getMinutes()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
 
-function displayForecast() {
+  return `${day}, ${hours}:${minutes}`;
+}
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let dys = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  dys.forEach(function (dy) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
           <div class="col-2">
-            <div class="weather-forecast-date">${dy}</div>
-            <img src="#" />
+            <div class="weather-forecast-date">${formatDay(
+              forecastDay.dt
+            )}</div>
+            <img 
+            src="http://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png" 
+            alt=""
+            width="42"
+            />
             <div class="weather-forecast-temperature">
-              <span class="weather-forecast-temperature-max">38° </span>
-              <span class="weather-forecast-temperature-min">30° </span>
+              <span class="weather-forecast-temperature-max"> ${Math.round(
+                forecastDay.temp.max
+              )}° </span>
+              <span class="weather-forecast-temperature-min">${Math.round(
+                forecastDay.temp.min
+              )}° </span>
             </div>
           </div>
         `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
 function getForecast(coordinates) {
-  console.log(coordinates);
-  let apiKey = "08cffde9ad7a38f4c307f61119d5c0cd";
+  let apiKey = "ca0db41e2e878c74a1dfc7ffece370d4";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
 }
+
 //
 function showResponse(response) {
   let cityName = document.querySelector("#city");
@@ -71,6 +79,7 @@ function showResponse(response) {
   let windElement = document.querySelector("#windval");
   let descripElement = document.querySelector("#description");
   let icon = document.querySelector("#icon");
+  let dateElement = document.querySelector("#sun");
   let temperatureValue = Math.round(response.data.main.temp);
   let humidityValue = Math.round(response.data.main.humidity);
   let windValue = Math.round(response.data.wind.speed);
@@ -81,6 +90,7 @@ function showResponse(response) {
   windElement.innerHTML = windValue;
   descripElement.innerHTML = descriptionValue;
   cityName.innerHTML = cityValue;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 
   celciusTemperature = response.data.main.temp;
 
@@ -95,7 +105,7 @@ function showResponse(response) {
 }
 
 function searchCity(city) {
-  let apiKey = "08cffde9ad7a38f4c307f61119d5c0cd";
+  let apiKey = "ca0db41e2e878c74a1dfc7ffece370d4";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showResponse);
 }
@@ -131,21 +141,20 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 searchCity("Brownsburg");
-displayForecast();
 
 //
 
-function searchLocation(position) {
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let apiKey = "08cffde9ad7a38f4c307f61119d5c0cd";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showResponse);
-}
+//function searchLocation(position) {
+//let lat = position.coords.latitude;
+//let lon = position.coords.longitude;
+//let apiKey = "ca0db41e2e878c74a1dfc7ffece370d4";
+//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+//axios.get(apiUrl).then(showResponse);
+//}
 
-function getCurrentPosition(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-let currentCity = document.querySelector("#current-button");
-currentCity.addEventListener("click", getCurrentPosition);
+//function getCurrentPosition(event) {
+//event.preventDefault();
+//navigator.geolocation.getCurrentPosition(searchLocation);
+//}
+//let currentCity = document.querySelector("#current-button");
+//currentCity.addEventListener("click", getCurrentPosition);
